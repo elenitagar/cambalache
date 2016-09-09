9cc648e04cc997e41419babbbef457ffd4558bf0
gunderwulde <gunderwulde@hotmail.es>
2016-09-09 12:08:57 +0200

    Update Block_Tagmanager.php

--- Block_Tagmanager.php
+++ Block_Tagmanager.php
@@ -247,7 +247,7 @@ class Panalysis_TagManager_Block_Tagmanager extends Mage_Core_Block_Template
                         'id' => $sku,
                         'variant' => $productId,
 //                        'price' => number_format($item_price, 2),
-                        'price' => $item->getPrice(),
+                        'price' => $product->getPrice(),
                         'quantity' => (int)$item->getQtyOrdered(),
                         'category' => $helper->getSkuCategory($sku)
                     );
