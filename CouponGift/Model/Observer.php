46df8e245fb242b96a3f1e9a0a5ed834ec7fd311
gunderwulde <gunderwulde@hotmail.es>
2016-09-16 14:04:24 +0200

    Update Observer.php

--- CouponGift/Model/Observer.php
+++ CouponGift/Model/Observer.php
@@ -75,7 +75,7 @@ class Sashas_CouponGift_Model_Observer {
 		$gift_products_sku_arr = explode( ',',  $rule->getGiftProductSku () );	
 		foreach ( $gift_products_sku_arr as $gift_product_sku ) {
 			SalesRuleAddProduct( $gift_product_sku, $force_price);
-			
+		}
 		return $this;
 	}
 
