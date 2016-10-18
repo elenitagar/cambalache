d8d0b78947d64d7e3262af1f296de982ed019700
gunderwulde <gunderwulde@hotmail.es>
2016-10-18 16:57:27 +0200

    Update ecommerce.php

--- ecommerce.php
+++ ecommerce.php
@@ -1,21 +1,25 @@
 <script>
-<?php if($_SERVER[SCRIPT_NAME]=="/success.html"): ?>	
+<?php 
+    $order = Mage::getSingleton('sales/order');
+    $order->load(Mage::getSingleton('checkout/session')->getLastOrderId());
+    if($_SERVER[SCRIPT_NAME]=="/success.html"): 
+?>	
 dataLayer = [{
-    'transactionId': '<?php echo  $transactions["trans_id"] ?>', // Transaction ID - Type:String - Required 
-    'transactionAffiliation': '<?php echo  $transactions["store_name"] ?>', // store name - Type:String - Optional to use
-    'transactionTotal': <?php echo  $transactions["revenue"] ?>, //total revenue - Type:Numeric - Required
-    'transactionTax': <?php echo  $transactions["tax"] ?>, // Tax amount for transaction - Type:Numeric - Optional to use
-    'transactionShipping': <?php echo  $transactions["shipping_cost"] ?>, // Shipping cost - Type:Numeric - Optional to use
+    'transactionId': '<?php echo $order->getIncrementId() ?>', // Transaction ID - Type:String - Required 
+    'transactionAffiliation': '<?php echo Mage::app()->getStore()->getName() ?>', // store name - Type:String - Optional to use
+    'transactionTotal': <?php echo $order->getGrandTotal() ?>, //total revenue - Type:Numeric - Required
+    'transactionTax': <?php echo $order->getTaxAmount() ?>, // Tax amount for transaction - Type:Numeric - Optional to use
+    'transactionShipping': <?php echo $order->getShippingAmount() ?>, // Shipping cost - Type:Numeric - Optional to use
     'transactionProducts': [
-    <?php for ($i=0;$n=sizeof($products_array);$i<$n;$i++): ?>
+    <?php  foreach ($order->getAllVisibleItems() as $item): ?>
 	{
-        'sku': '<?php echo $products_array[$i]["sku"] ?>', // Product SKU - Type:String - Required 
-        'name': '<?php echo $products_array[$i]["name"] ?>', // Product Name - Type:String - Required 
-        'category': '<?php echo $products_array[$i]["category"] ?>', // Product Category - Type:String - Optional to use
-        'price': <?php echo $products_array[$i]["price"] ?>, // Product Price - Type:Numeric - Required 
-        'quantity': <?php echo $products_array[$i]["quantity"] ?> // Product Quantity - Type:Numeric - Required 
+        'sku': '<?php echo $item->getSku() ?>', // Product SKU - Type:String - Required 
+//        'name': '<?php echo $products_array[$i]["name"] ?>', // Product Name - Type:String - Required 
+//        'category': '<?php echo $products_array[$i]["category"] ?>', // Product Category - Type:String - Optional to use
+//        'price': <?php echo $products_array[$i]["price"] ?>, // Product Price - Type:Numeric - Required 
+        'quantity': <?php echo (int)$item->getQty() ?> // Product Quantity - Type:Numeric - Required 
 	},
-    <?php endfor; ?>
+    <?php endforeach; ?>
      ]
     }];
 <?php endif; ?>  
